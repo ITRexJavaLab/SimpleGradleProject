7dcc63c0dd5c65d59bc4b1c781554ef1672b1ad5
Dima Polianski <contr.dima.polianski@pandadoc.com>
2021-10-08 10:10:37 +0300

    Initial commit

---
+++ src/main/java/com/itrex/java/lab/Main.java
@@ -0,0 +1,16 @@
+package com.itrex.java.lab;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("-----------start----------\n");
+        System.out.println("Hello world from gradle");
+
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+
+        System.out.println("\n-----------end----------\n");
+    }
+
+}
